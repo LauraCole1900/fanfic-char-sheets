148df2f9eb42b46406ac6f740c84661832043b9a
Laura Cole <lauracole1900@comcast.net>
2024-04-02 03:17:05 -0600

    added Mutations to typeDefs

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -2,6 +2,32 @@ const typeDefs = `#graphql
 
 scalar Date
 
+input CharacterInput {
+  userId: ID!
+  firstName: String!
+  nickName: String
+  middleName: String
+  lastName: String!
+  gender: String!
+  race: String!
+  birthDate: Date!
+  marriedDate: Date
+  deathDate: Date
+  birthLoc: String!
+  marriedLoc: String
+  deathLoc: String
+  fatherId: ID
+  motherId: ID
+  spouseId: ID
+  milBranch: String
+  occupation: String
+  liveBirth: Boolean!
+  miscarriage: Boolean!
+  lifeNotes: String
+  deathNotes: String
+  fandom: [String!]!
+}
+
 type Character {
   id: ID!
   userId: ID!
@@ -35,10 +61,22 @@ type User {
   password: String!
 }
 
+type Auth {
+  token: ID!
+  user: User
+}
+
 type Query {
   me: User
   allChars: [Character]
   singleChar(id: ID!): Character
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  createChar(input: CharInput!): Character
+  updateChar(id: ID! input: CharInput!): Character
+  deleteChar(id: ID!): Character
 }
 `;
 
